1fda11bb040dca7955ccd363a4630a928c5b9f54
Abhinav Pratap Singh <abhinav.ism.iit@gmail.com>
2024-01-23 17:38:21 -0500

    type stability

--- src/MT.jl
+++ src/MT.jl
@@ -1,29 +1,30 @@
 module MT
 using Optim, LinearAlgebra
 using Plots
-mutable struct model
-    m::Vector{Float64}
-    h::Vector{Float64}
+
+mutable struct model{T <: Union{AbstractVector{Float32}, AbstractVector{Float64}}}
+    m::T
+    h::T
 end
 
-mutable struct response
-    ρₐ::Vector{Float64}
-    ϕ::Vector{Float64}
+mutable struct response{T <: Union{AbstractVector{Float32}, AbstractVector{Float64}}}
+    ρₐ::T
+    ϕ::T
 end
 
-function response(ω::Vector{T}) where T <: Union{Float32, Float64}
-    return response(
-        zero(ω),
-        zero(ω)
-    )
-end
+# function response(ω::Vector{T}) where T <: Union{Float32, Float64}
+#     return response(
+#         zero(ω),
+#         zero(ω)
+#     )
+# end
 
 include("fwd_1d.jl")
 include("plots.jl")
 include("inv.jl")
 
 # global ut::utils
-export μ
+export μ, PRECISION
 export model, response
 export plot_response, prepare_plot, prepare_plot!
 export plot_model, plot_model!
